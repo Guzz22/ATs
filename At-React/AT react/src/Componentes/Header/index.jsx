73ed9308a42826f423103c50ee25a370ba3cec8d
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-17 16:31:43 -0300

    AT-React

---
+++ At-React/AT react/src/Componentes/Header/index.jsx
@@ -0,0 +1,13 @@
+import {Link} from "react-router-dom"
+ 
+export default function Header(){
+    return(
+        <>
+        <div className="Cabeca">
+            <Link to="/">Home</Link>
+            <Link to="sobre">Sobre</Link>
+            <Link to="contato">Contato</Link>
+        </div>
+        </>
+    )
+}
---
+++ At-React/AT react/src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import ConteudoPrincipal from "../Componentes/ConteudoPincipal"
+import Cards from "../Componentes/Cards";
+ 
+export default function Home(){
+    return(
+        <>
+        <ConteudoPrincipal/>
+        <Cards/>
+        </>
+    );
+}
